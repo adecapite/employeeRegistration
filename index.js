ba124dd80f40036f383712c4ecdd223f15807718
Anthony DeCapite <anthony.decapite@icloud.com>
2021-04-01 17:59:30 -0400

    created variables for each type of employee and index.

---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require('inquirer')
+const fs = require('fs')
+const employee = require('./library/employee')
+const manager = require("./library/manager")
+const enginner = require('./library/engineer')
+const intern = require("./library/intern")
