8576903734c373e4fba778c2c46188ffc1b853a0
Anthony DeCapite <anthony.decapite@icloud.com>
2021-04-01 18:28:51 -0400

    started ewo0rking on prompts.

--- index.js
+++ index.js
@@ -4,3 +4,25 @@ const employee = require('./library/employee')
 const manager = require("./library/manager")
 const enginner = require('./library/engineer')
 const intern = require("./library/intern")
+const { type } = require('os')
+
+function startApp(){
+    startPage();
+    addEmployee();
+}
+
+function addEmployee(){
+    inquirer.prompt([{
+        type: 'input',
+        message: "Enter employee's name.",
+        name: 'employeeName' 
+    },
+    {
+        type: 'list',
+        message:  
+    }
+        
+    ]
+
+    )
+}
